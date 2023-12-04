bd958f7438817791d090d92fd1e05a945555f957
David Hisel <132942678+davidh-cyberark@users.noreply.github.com>
2023-12-04 15:20:11 -0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+// This is a GG sandbox file ... creds are intentionally going to be put in here
+// while developing an integration with GG
+
+func main() {
+  // printf "hello world" | sha256sum 
+  myFakeSecret := "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"
+}
