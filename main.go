0e36ef5c4ace9a49d2e14cc975ec50c01f720837
David Hisel <132942678+davidh-cyberark@users.noreply.github.com>
2023-12-29 15:09:47 -0800

    Update main.go add 2 more

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ package main
 func main() {
 
   // $ printf "hello world - 003" | sha256sum 
-  myFakeSecret := "6db2e4f95fa902795b6054fdf8567c7038d36842a9c2305090b1729f27a9f0d4"
+  myFakeSecret1 := "6db2e4f95fa902795b6054fdf8567c7038d36842a9c2305090b1729f27a9f0d4"
+  // $ printf "hello world - 004" | sha256sum 
+  myFakeSecret2 := "179811f8a95cbfe8856f6029db9ba3f827a936877d0090ca8342578f23877010"
+  // $ printf "hello world - 005" | sha256sum 
+  myFakeSecret3 := "243a0e86e3e09435e85b89e664f865cd3cce9b0c968d4e6a17f9bde12a247cf4"
 }
 
