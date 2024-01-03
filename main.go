ca91f27b93c82b2ef7535d32c09d77b2b086477a
David Hisel <132942678+davidh-cyberark@users.noreply.github.com>
2024-01-02 19:04:24 -0800

    Update main.go num 15

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ package main
 // while developing an integration with GG
 
 func main() {
-  // $ printf "hello world - 014" | sha256sum 
-  myFakeSecret3 := "47a79a9b9384b49067d568264916b9b1f13e91fb2f9069b7ccf11515d300d948"
+  // $ printf "hello world - 015" | sha256sum 
+  myFakeSecret3 := "25448e40c2f12e4bfca8041163c502a4a45079e1c13cf6b95f3a8f4e96b9f339"
 }
 
