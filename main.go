564affa2b3e4570b4f10e5e50a7e49a8f52303d0
David Hisel <132942678+davidh-cyberark@users.noreply.github.com>
2024-01-02 18:51:36 -0800

    Update main.go num 10

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
   myFakeSecret1 := "6db2e4f95fa902795b6054fdf8567c7038d36842a9c2305090b1729f27a9f0d4"
   // $ printf "hello world - 004" | sha256sum 
   myFakeSecret2 := "179811f8a95cbfe8856f6029db9ba3f827a936877d0090ca8342578f23877010"
-  // $ printf "hello world - 009" | sha256sum 
-  myFakeSecret3 := "5893b72941beb20ce3122b3d6489d3022775e80e38e9aa16c1dd2b4edcfb9f87"
+  // $ printf "hello world - 010" | sha256sum 
+  myFakeSecret3 := "9321c067b5a7f9b44a9527b19f697a54aa44c34c59a890571375e67acaf05abd"
 }
 
