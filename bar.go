58c30df7a122a036c7548446cf80acfed9d23667
David Hisel <132942678+davidh-cyberark@users.noreply.github.com>
2024-01-04 13:41:22 -0800

    Update bar.go 23 again again

--- bar.go
+++ bar.go
@@ -1,5 +1,16 @@
 package bar
 
+type Client struct {
+  User string
+  Pass string
+}
+
 func bar() {
-  anothertestsecret := "2f2aafa267574197c7b7737f9fc93906452ca47525439c3f7ec1addfd27a842c"
+  client := Client{
+    User: "helloworld"
+    Pass: "2f2aafa267574197c7b7737f9fc93906452ca47525439c3f7ec1addfd27a842c"
+  }
+
+  log.Printf("Client credentials: %s/%s\n", client.User, client.Pass)
+    
 }
