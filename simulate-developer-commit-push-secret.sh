b57a0e465f7d59bff45cb9d6f8f8d01e1ed558dd
David Hisel <david.hisel@cyberark.com>
2024-01-05 19:05:17 +0000

    add script to simulate dev commit/push

---
+++ simulate-developer-commit-push-secret.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash 
+
+export MY_TIMESTAMP=${EPOCHSECONDS:-$(date +%s)}
+if [ -z "$MY_TIMESTAMP" ]; then
+    echo "Could not create timestamp, exiting."
+    exit 1
+fi
+
+export HASH="$(printf 'hello world - %s' $MY_TIMESTAMP | sha256sum | awk '{print $1}')"
+if [ -z "$HASH" ]; then
+    echo "Empty hash, exiting."
+    exit 2
+fi
+
+read -r -d '' TMPL <<-EOF
+	package main
+	
+	// This is a GG sandbox file ... creds are intentionally going to be put in here
+	// while developing an integration with GG
+	
+	func main() {
+	  // $ printf "hello world - $MY_TIMESTAMP" | sha256sum 
+	  myFakeSecret := "$HASH"
+	}
+EOF
+
+eval "echo '$TMPL' > main.go"
+
+git add main.go
+git commit -m "rev $TS"
+git push
+
