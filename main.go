2c9204462da248f10f61429cbfa9bf0186ed604e
David Hisel <david.hisel@cyberark.com>
2024-01-08 16:27:44 +0000

    rev

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 // while developing an integration with GG
 
 func main() {
+  // $ HASH_VARNAME=5f6dca980db270e0c481888c8344e10f3868dc6425bc1d759ed409cd247b8f21  -
+
   myFakeSecret, ok := os.LookupEnv("HASH_VARNAME")
   if !ok {
     panic("failed to lookup hash env var")
