1c17b2840fc6a900985da97ba64cfd958413cabb
David Hisel <david.hisel@cyberark.com>
2024-01-08 16:32:00 +0000

    rev

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
-import (
-"os"
-"log"
-)
 
 // This is a GG sandbox file ... creds are intentionally going to be put in here
 // while developing an integration with GG
 
 func main() {
-  // $ HASH_VARNAME=$(printf "hello world - 1704731452" | sha256sum)
-
-  myFakeSecret, ok := os.LookupEnv("HASH_VARNAME")
-  if !ok {
-    panic("failed to lookup hash env var")
-  }
-  log.Println(myFakeSecret)
+  // $ printf "hello world - 1704731520" | sha256sum 
+  myFakeSecret := "6b94a518254527ed404e4a7f1701af7edc76956c22673b62cb5fc708b0540e5d"
 }
