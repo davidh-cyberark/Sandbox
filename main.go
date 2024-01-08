7b7bd29f12ceb3503d1de03394526876309b06ab
David Hisel <david.hisel@cyberark.com>
2024-01-08 16:17:49 +0000

    rev

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
+import "os"
 
 // This is a GG sandbox file ... creds are intentionally going to be put in here
 // while developing an integration with GG
 
 func main() {
-  // $ printf "hello world - 1704730595" | sha256sum 
-  myFakeSecret, e := os.LookupEnv("HASH_VARNAME")
+  // $ printf "hello world - 1704730669" | sha256sum 
+  if myFakeSecret, e := os.LookupEnv("HASH_VARNAME"); e != nil {
+    panic(e)
+  }
 }
