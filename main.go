b6ca7c53f09ff049ccc7f4ca5d02443694d6cd17
David Hisel <david.hisel@cyberark.com>
2024-01-09 23:03:48 +0000

    rev 1704841428

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 // while developing an integration with GG
 
 func main() {
-  // $ HASH_VARNAME=$(printf "hello world - 1704841427" | sha256sum)
+  // $ HASH_VARNAME=$(printf "hello world - 1704841428" | sha256sum)
 
   myFakeSecret, ok := os.LookupEnv("HASH_VARNAME")
   if !ok {
