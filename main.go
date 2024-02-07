a7b3fbb5ceafa5d3f27244b1039e57d932b46a83
David Hisel <David.Hisel@CyberArk.com>
2024-02-07 22:34:18 +0800

    rev 1707316457

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 // while developing an integration with GG
 
 func main() {
-  // $ printf "hello world - 1704904598" | sha256sum 
-  myFakeSecret := "aa372794bc12b174483d4e48131e88388b443af3f3cda4939a16fa23dd66020a"
+  // $ printf "hello world - 1707316457" | sha256sum 
+  myFakeSecret := "a111ec8f10c9d4bee85d79c800a2d545bde8e957db4fe90116369cc09a2b0c11"
   log.Println(myFakeSecret)
 }
