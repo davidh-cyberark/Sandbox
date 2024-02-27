6dd391b51142cc13799c83b1d86377d12f914fe0
David Hisel <david.hisel@cyberark.com>
2024-02-27 21:08:16 +0000

    rev 1709068096

--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"log"
+	"os"
 )
 
 // This is a GG sandbox file ... creds are intentionally going to be put in here
 // while developing an integration with GG
 
 func main() {
-	// $ printf "hello world - 1709067850" | sha256sum
-	myFakeSecret := "ef76a7514d37c1891655b26c0f4083bc3eb29a2218d7605e3a3277013a89c416"
+	// $ HASH_VARNAME=$(printf "hello world - 1707316681" | sha256sum)
+
+	myFakeSecret, ok := os.LookupEnv("HASH_VARNAME")
+	if !ok {
+		panic("failed to lookup hash env var")
+	}
+
+	// for testing GH secret scanning
+	aws_key = "AKIAW5PALCL65B5MNJ6C"
+	aws_secret = "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"
+
 	log.Println(myFakeSecret)
 }
