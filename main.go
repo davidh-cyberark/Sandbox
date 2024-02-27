afdeb4f984b8ac7f8089044618c20c63dad302a3
David Hisel <david.hisel@cyberark.com>
2024-02-27 20:44:48 +0000

    rev 1709066688

--- main.go
+++ main.go
@@ -8,16 +8,11 @@ import (
 // while developing an integration with GG
 
 func main() {
-  // $ HASH_VARNAME=$(printf "hello world - 1707316681" | sha256sum)
+  // $ HASH_VARNAME=$(printf "hello world - 1709066688" | sha256sum)
 
   myFakeSecret, ok := os.LookupEnv("HASH_VARNAME")
   if !ok {
     panic("failed to lookup hash env var")
   }
-
-  // for testing GH secret scanning
-  aws_key="AKIAW5PALCL6Z2L47ZNR"
-  aws_secret="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"
-
   log.Println(myFakeSecret)
 }
