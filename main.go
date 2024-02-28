a59ddd9c97a16dec08ceb7fa75f41012a0d3609d
David Hisel <david.hisel@cyberark.com>
2024-02-28 03:03:35 +0000

    rev 1709089415

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
-
 import (
-	"log"
+"log"
 )
 
 // This is a GG sandbox file ... creds are intentionally going to be put in here
 // while developing an integration with GG
 
 func main() {
-	// $ printf "hello world - 1709089297" | sha256sum
-	mySecret := "884d8d60becd286564645415b40aaf6eb05747905f581f4cbeeb00f4163c1f78"
-	log.Println(mySecret)
+  // $ printf "hello world - 1709089415" | sha256sum 
+  mySecret := "e90af70030663ccbc67a4cb38edd23668c220d5f4338f868f216790ef6d4e7b1"
+  log.Println(mySecret)
 }
