7ff176f48f09259a25ea238e20117cd97b013b2f
David Hisel <david.hisel@cyberark.com>
2024-02-28 04:14:50 +0000

    rev 1709093690

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 // while developing an integration with GG
 
 func main() {
-  // $ printf "hello world - 1709093629" | sha256sum 
-  secret := "QFKoh3tCyLdHq7OwXbTO4hoYrwmBVflNQgesspf2xOfF+JcPjecLpIXnIAVh+-1i"
+  // $ printf "hello world - 1709093690" | sha256sum 
+  secret := "CyberArk123"
   log.Println(secret)
 }
