5bd872f07a9a0dd3409e313445971b0e29930841
David Hisel <david.hisel@cyberark.com>
2024-02-28 03:26:55 +0000

    rev 1709090815

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 import (
-"os"
 "log"
 )
 
@@ -8,11 +7,7 @@ import (
 // while developing an integration with GG
 
 func main() {
-  // $ HASH_VARNAME=$(printf "hello world - 1709090787" | sha256sum)
-
-  secret, ok := os.LookupEnv("HASH_VARNAME")
-  if !ok {
-    panic("failed to lookup hash env var")
-  }
+  // $ printf "hello world - 1709090815" | sha256sum 
+  secret := "84283bd307fab448b663bf828451306eac0d97171d7d6e268aed279213d417a4"
   log.Println(secret)
 }
