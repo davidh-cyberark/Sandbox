045424ac3ea74a9f6670af5389bda15714e733dd
David Hisel <david.hisel@cyberark.com>
2024-03-22 21:46:18 +0000

    try again with updated variable name

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 
 func main() {
 	username := "demouser1"
-	password := "ytLD51mDw-P-8PCil1A3pkEBlxODkIbLS0KVmkSAk7geRNxzl/Zh2/.fTVs46KS3"
+	secret := "ytLD51mDw-P-8PCil1A3pkEBlxODkIbLS0KVmkSAk7geRNxzl/Zh2/.fTVs46KS3"
 
-	if len(username) == 0 || len(password) == 0 {
+	if len(username) == 0 || len(secret) == 0 {
 		panic("username or password is not set")
 	}
-	log.Printf("username is %s, and password is %s\n", username, password)
+	log.Printf("username is %s, and password is %s\n", username, secret)
 }
