9d9ba967e325e99c436577de16deec59336932e1
David Hisel <david.hisel@cyberark.com>
2024-03-22 21:41:14 +0000

    rev 2024/03/22 21:41:14

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
-import (
-"os"
-"log"
-)
+
+// Updated: <2024/03/22 21:41:14>
 
 // This is a GG sandbox file ... creds are intentionally going to be put in here
 // while developing an integration with GG
 
-func main() {
-  // $ HASH_VARNAME=$(printf "hello world - 1709179145" | sha256sum)
+import (
+	"log"
+	"os"
+)
 
-  secret, ok := os.LookupEnv("HASH_VARNAME")
-  if !ok {
-    panic("failed to lookup hash env var")
-  }
-  log.Println(secret)
+func main() {
+	if username, ok := os.LookupEnv("SANDBOX_USERNAME"); !ok {
+		panic("failed to lookup env var")
+	}
+	if password, ok := os.LookupEnv("SANDBOX_PASSWORD"); !ok {
+		panic("failed to lookup env var")
+	}
+	if len(username) == 0 || len(password) == 0 {
+		panic("username or password is not set")
+	}
+	log.Printf("username is %s, and password is %s\n", username, password)
 }
