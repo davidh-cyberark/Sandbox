886ffba271879237a36a1bf7cab7919aefbb710d
David Hisel <132942678+davidh-cyberark@users.noreply.github.com>
2024-03-25 08:01:36 -0700

    clean12

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	var password string
-	password = "JYSvcmVSZXBvcnRpbmdFjdispoUyMDIycGVyaXNjb3q%"
+	password = "TODO"
 
 	if len(password) == 0 {
 		panic("password is not set")
