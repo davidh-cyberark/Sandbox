c0848007eb6db230c12b827cbdd76589e28cf780
David Hisel <david.hisel@cyberark.com>
2024-03-26 00:42:23 +0000

    rev 2024/03/26 00:42:23

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-// Updated: <2024/03/26 00:41:46>
+// Updated: <2024/03/26 00:42:23>
 
 // This is a GG sandbox file ... creds are intentionally going to be put in here
 // while developing an integration with GG
@@ -9,10 +9,8 @@ import (
 	"log"
 )
 
-
 func main() {
-	var password string
-	password = "TODO add this"
+	password := "CkrXHE~GRg2PwmmywRIQdDBSCLi3lxec~HFvRKlD/qsu6TksIGbHTc8msio_3F1+"
 
 	if len(password) == 0 {
 		panic("password is not set")
