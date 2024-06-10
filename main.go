f016da5a91ea44384b4255231015d5e269b6288b
David Hisel <david.hisel@cyberark.com>
2024-06-10 13:09:27 +0000

    rev 2024/06/10 13:09:27

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-// Updated: <2024/06/10 13:07:49>
+// Updated: <2024/06/10 13:09:27>
 
 // This is a GG sandbox file ... creds are intentionally going to be put in here
 // while developing an integration with GG
